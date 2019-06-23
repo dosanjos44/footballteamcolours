b31bf80c007bb7c2d13f93a57507013fd228ca20
dosanjos44 <jamie.dosanjos@gmail.com>
2019-06-23 21:22:13 +0100

    Updates

---
+++ precache-manifest.2e852a77a133423163c0d0808f9eaeb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bb69b52fe930210aae6",
+    "url": "/footballteamcolours/static/js/runtime~main.7bb69b52.js"
+  },
+  {
+    "revision": "5d00a4414bed2ac31b50",
+    "url": "/footballteamcolours/static/js/main.5d00a441.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/footballteamcolours/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "5d00a4414bed2ac31b50",
+    "url": "/footballteamcolours/static/css/main.8d37976d.chunk.css"
+  },
+  {
+    "revision": "4604e9ee37772f73a3d53679386c8b88",
+    "url": "/footballteamcolours/index.html"
+  }
+];
